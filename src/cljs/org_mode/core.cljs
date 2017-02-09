16fc039351b05ada4bc4cc2be59c00367d2d2fa7
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2017-02-09 08:37:11 +0200

    hide elements
    - show on hover

--- src/cljs/org_mode/core.cljs
+++ src/cljs/org_mode/core.cljs
@@ -1,10 +1,42 @@
 (ns org-mode.core
-   (:require [dommy.core :refer-macros [sel sel1]]))
+   (:require [dommy.core :as dommy :refer-macros [sel sel1]]))
 
 (enable-console-print!)
 
+(defn set-visibility [el value]
+  (set! (.. el -style -display) value))
+
+(defn toggle-visibility [el]
+  (let [value (.. el -style -display)]
+    (if (= value "none")
+      (set-visibility el "initial")
+      (set-visibility el "none"))))
+
+(defn hide-all-but-first-article []
+  (let [articles (drop 1 (sel :article))]
+    (doseq [article articles]
+      (set! (.. article -style -display) "none"))))
+
+(defn hide-all-articles []
+  (doseq [article (sel :article)]
+    (set! (.. article -style -display) "none"))
+  )
+(defn hide-all-but-article [el]
+  (hide-all-articles)
+  (toggle-visibility el))
+
+(defn handler [e]
+  (let [hash (.. e -target -hash)]
+    (hide-all-but-article (sel1 (keyword (str "#outline-container-" (subs hash 1)))))) )
+
+(defn attach-hover-listeners []
+  (let [els (sel [:#table-of-contents :li :a])]
+    (doseq [el els]
+      (dommy/listen! el :mouseover handler :click handler))))
+
 (defn init []
-  (println "Hello, world"))
+  (hide-all-but-first-article)
+  (attach-hover-listeners))
 
 (set! (.-onload js/window) init)
 
